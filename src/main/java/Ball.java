324a4e2f538f0357fb3cf0577b191ff05ea6a953
Mattia Romeo <mattiaromeo@gmail.com>
2017-10-23 13:47:32 +0200

    commited to these balls

--- src/main/java/Ball.java
+++ src/main/java/Ball.java
@@ -1,11 +1,48 @@
 public class Ball {
     private static Ball ball;
     private String color;
-    private Ball (String color) { this.color = color;}
-    public void bounce() { System.out.println("boing!"); }
-    public static Ball getInstance (String color) {
-        if (ball == null) { ball = new Ball(color); }
+
+    private Ball(String color) {
+        this.color = color;
+    }
+
+    public void bounce() {
+        System.out.println("boing!");
+    }
+
+    public static Ball getInstance(String color) {
+        if (ball == null) {
+            ball = new Ball(color);
+        } else {
+            if (ball.getColor().equals(color)) {
+                return ball;
+            }
+            else{
+                ball = new Ball(color);
+            }
+        }
+
         return ball;
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Ball ball = (Ball) o;
+
+        return color != null ? color.equals(ball.color) : ball.color == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return color != null ? color.hashCode() : 0;
     }
 }
 
--- src/test/java/BallTest.java
+++ src/test/java/BallTest.java
@@ -1,17 +1,28 @@
-import org.junit.Test;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 
 
 public class BallTest {
     @Test
     public void ballWithParameterBlue_shouldReturnBlueBall() throws Exception {
-
+        Ball blue = Ball.getInstance("Blue");
+        Assertions.assertThat(blue.getColor()).isEqualTo("Blue");
 
     }
 
     @Test
     public void anotherBallWithParameterBlue_shouldReturnSameBlueBall() throws Exception {
 
+        Ball blue = Ball.getInstance("Blue");
+        Ball red = Ball.getInstance("RED");
+        Assertions.assertThat(red.getColor()).isEqualTo("RED");
 
+    }
+
+    @Test
+    public void ballWithParameterRed_ShouldReturnRedBall() throws Exception {
+        Ball red = Ball.getInstance("RED");
+        Assertions.assertThat(red.getColor()).isEqualTo("RED");
     }
 }
